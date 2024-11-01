fcb18258d216afbc173fc01c080aafeb79874e39
Elmurod Torayev <elmuroddevvv@gmail.com>
2024-11-01 14:33:31 +0500

    Added DbContext File and DataAccess project
    Added DbContext File and DataAccess project

---
+++ AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StaffManagement.DataAccess.Models;
+using StaffManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffManagement.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Staff> Staffs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
+    }
+}
